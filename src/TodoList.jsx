0c6c13f1a3084a1bc0075b4316773dc62538e7b1
kratik <kratik38@gmail.com>
2023-05-11 17:03:35 +0530

    a basic todo list page using basic react

---
+++ src/TodoList.jsx
@@ -0,0 +1,22 @@
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({ todos,toggleTodo,deleteTodo }){
+
+	return (
+		<ul className='list'>
+
+    {todos.length===0 && "No Todos"}
+
+    {todos.map(todo=>{
+      return (
+      <TodoItem
+      toggleTodo={toggleTodo}
+      deleteTodo={deleteTodo}   
+      {...todo}
+      key={todo.id}
+      />
+    )
+    })}
+  </ul>  
+	)
+}
